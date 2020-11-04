2446fc8f8a9f4702754513c68fe199a47e3d7154
Gokul vipin <gvipin@stu.naperville203.org>
2020-11-04 09:59:40 -0600

    Hard coded Annuity class

---
+++ Annuity2.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class Annuity2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Annuity2
+{
+    public static void main(String[] args){
+        System.out.print(Annuity.calcPresentValue(10000, 0.08, 20));
+    }
+}
