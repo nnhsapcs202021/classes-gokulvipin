649f6fffda0ed6a74358d5e1a4d689827cd5ced6
Gokul vipin <gvipin@stu.naperville203.org>
2020-11-13 10:28:31 -0600

    This has the draw method for sun

--- Sun.java
+++ Sun.java
@@ -1,3 +1,5 @@
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 
 /**
  * Write a description of class Sun here.
@@ -6,17 +8,22 @@
  * @version (a version number or a date)
  */
 public class Sun{
-        private int radius ;
-        private String color;
-        private Sun sun1;   
-      
-     /**
+    private double radius ;
+    private String color;
+    private Sun sun1;   
+
+    /**
      * Default constructor for the Sun class.
      */
     public Sun(int initialRadius, String initialColor){
-       this.radius = initialRadius;
-       this.color = initialColor;  
+        this.radius = initialRadius;
+        this.color = initialColor;  
+    }
+    public void draw(Graphics2D g2)
+   {
+      //Ellipse2D.Double frontTire 
+       //  = new Ellipse2D.Double(this.radius);
    }
-        
-   }
+
+}
 
